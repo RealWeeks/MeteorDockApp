f47d1229ddfacda2b2f75b687eabdcd59b19bf16
J-Weeks <weeksjasons@gmail.com>
2015-04-05 13:51:56 -0400

    switched to guardian test API for news source.  Added text box for commenting, but not yet live. need to access invis session storage

--- client/home/home.js
+++ client/home/home.js
@@ -1,9 +1,13 @@
-Template.layout.currentnews = function(){
+Template.home.currentnews = function(){
   var news = Session.get('news');
-  Meteor.call('getNews',news,function(err, results){
-    console.log(results.content);
-    console.log('hello from currentnews function');
-    Session.set('news', JSON.parse(results.content));
+  Meteor.call('getNews',news,function(err, guardian){
+    // var snip = JSON.parse(guardian.content);
+    // console.log(snip);
+    console.log(guardian.content);
+    // Session.set('news', snip);
+    // Session.set('news', guardian.content);
+    Session.set('news', 'test');
+
   });
   return Session.get('news');
 };
--- server/server.js
+++ server/server.js
@@ -1,1 +1,5 @@
-
+Meteor.methods({
+  'getNews':function(subject){
+    return Meteor.http.call('GET', 'http://content.guardianapis.com/search?api-key=test&q='+ subject);
+  }
+});
