a8bd507bf3d654e3b486dbc88602a79fabec4b45
J-Weeks <weeksjasons@gmail.com>
2015-04-06 11:16:33 -0400

    added html for users, comments and comments of comments, as well as seeded some trial images. Still need to add server side logic

--- client/home/home.js
+++ client/home/home.js
@@ -3,10 +3,10 @@ Template.home.currentnews = function(){
   Meteor.call('getNews',news,function(err, guardian){
     // var snip = JSON.parse(guardian.content);
     // console.log(snip);
-    console.log(guardian.content);
+    console.log(JSON.parse(guardian.content).response.results[2].webTitle);
     // Session.set('news', snip);
     // Session.set('news', guardian.content);
-    Session.set('news', 'test');
+    Session.set('news', 'asdf');
 
   });
   return Session.get('news');
