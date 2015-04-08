7cec4187153573d11c2b47b37eee1b49708efff0
J-Weeks <weeksjasons@gmail.com>
2015-04-08 14:46:45 -0400

    multiuser 1v1 chat working

--- client/dock/dock.js
+++ client/dock/dock.js
@@ -14,3 +14,54 @@ Template.dock.helpers({
     //$ne - mongo for !=
   }
 });
+
+Template.dock.events({
+  'click .user':function(){
+    Session.set('currentId',this._id);
+    var result = ChatRooms.findOne({chatIds:{$all:[this._id,Meteor.userId()]}});
+    //findOne, reterns a single entry from Mongo
+    //$all - returns all docs(read:postgres tables) in following array
+    //http://docs.mongodb.org/manual/reference/operator/query/all/
+    //looks for room already created
+    if(result){
+      Session.set('roomid',result._id);
+    }else{
+      var newRoom = ChatRooms.insert({chatIds:[this._id,Meteor.userId()],messages:[]});
+      Session.set('roomid',newRoom);
+    }
+  }
+});
+
+Template.messages.helpers({
+  'msgs':function(){
+    var res = ChatRooms.findOne({_id:Session.get('roomid')});
+    return res.messages;
+    //gets the messages for a chatroom by _id
+  }
+});
+
+Template.input.events={
+  'keydown input#message':function(event){
+    if(event.which === 13){
+      if(Meteor.user()){
+        var name = Meteor.user().username;
+        var message = document.getElementById('message');
+        //dom element not jquery object
+
+        if (message.value !== ''){
+          var upt = ChatRooms.update({"_id":Session.get("roomid")},{$push:{messages:{
+            name: name,
+            text: message.value,
+            createdAt: Date.now()
+          }}});
+          console.log(upt);
+          document.getElementById('message').value = "";
+          message.value ="";
+        }
+      }else{
+      alert("LOGIN!");
+      }
+      //checked to see if logged in ^
+    }
+  }
+};
